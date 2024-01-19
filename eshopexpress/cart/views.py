666712f5369f32458047396e0e9f1cffda633746
Andrey Frolov <senior.slender2015@gmail.com>
2024-01-19 19:11:22 +0200

    cart add, cart count

--- eshopexpress/cart/views.py
+++ eshopexpress/cart/views.py
@@ -2,13 +2,23 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from shop.models import ProductProxy
 
+from .cart import Cart
+
 
 def cart_view(request):
     return render(request, 'cart/cart-view.html')
 
 
 def cart_add(request):
-    ...
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        product = get_object_or_404(ProductProxy, id=product_id)
+        cart.add(product=product, quantity=product_qty)
+        cart_qty = cart.__len__()
+        response = JsonResponse({'qty': cart_qty, "product": product.title})
+        return response
 
 
 def cart_delete(request):

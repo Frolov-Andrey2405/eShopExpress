f36a1943d6723f0699afb0087b97004a53f56036
Andrey Frolov <senior.slender2015@gmail.com>
2024-01-21 12:43:39 +0200

    Two-factor authentication, sending mail, creating/registering an account

--- eshopexpress/account/forms.py
+++ eshopexpress/account/forms.py
@@ -3,8 +3,7 @@ from typing import Any
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
-# from django.forms.widgets import PasswordInput, TextInput
+from django.forms.widgets import PasswordInput, TextInput
 
 User = get_user_model()
 
@@ -32,8 +31,22 @@ class UserCreateForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    ...
+    username = forms.CharField(
+        widget=TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(
+        widget=PasswordInput(attrs={'class': 'form-control'}))
 
 
 class UserUpdateForm(forms.ModelForm):
-    ...
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].label = 'Your Email Address'
+        self.fields['email'].required = True
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        exclude = ('password1', 'password2')
--- eshopexpress/account/urls.py
+++ eshopexpress/account/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.urls import path
 
 from . import views
@@ -5,5 +6,20 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+    # Registration and verification
     path('register/', views.register_user, name='register'),
+    path(
+        'email-verification-sent/', lambda request: render(
+            request, 'account/email/email-verification-sent.html'),
+        name='email-verification-sent'
+    ),
+
+    # Login & Logout
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
+    # Dashboard
+    path('dashboard/', views.dashboard_user, name='dashboard'),
+    path('profile-management/', views.profile_user, name='profile-management'),
+    path('delete-user/', views.delete_user, name='delete-user'),
 ]

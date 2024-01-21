8e8f3ed7a9e041cf8e4a68ec7f7f626fe9c46a63
Andrey Frolov <senior.slender2015@gmail.com>
2024-01-21 13:21:50 +0200

    Shipping

--- eshopexpress/payment/urls.py
+++ eshopexpress/payment/urls.py
@@ -5,10 +5,9 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('payment-success/', views.payment_success, name='payment-success'),
-    path('payment-failed/', views.payment_failed, name='payment-failed'),
     path('shipping/', views.shipping, name='shipping'),
     path('checkout/', views.checkout, name='checkout'),
     path('complete-order/', views.complete_order, name='complete-order'),
-
+    path('payment-success/', views.payment_success, name='payment-success'),
+    path('payment-failed/', views.payment_failed, name='payment-failed'),
 ]
--- eshopexpress/payment/views.py
+++ eshopexpress/payment/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from cart.cart import Cart
+
+from .forms import ShippingAddressForm
+from .models import Order, OrderItem, ShippingAddress
+
+from django.http import JsonResponse
 
 
+@login_required(login_url='account:login')
 def shipping(request):
-    ...
+    try:
+        shipping_address = ShippingAddress.objects.get(user=request.user)
+    except ShippingAddress.DoesNotExist:
+        shipping_address = None
+
+    form = ShippingAddressForm(instance=shipping_address)
+
+    if request.method == 'POST':
+        form = ShippingAddressForm(request.POST, instance=shipping_address)
+        if form.is_valid():
+            shipping_address = form.save(commit=False)
+            shipping_address.user = request.user
+            shipping_address.save()
+            return redirect('account:dashboard')
+
+    return render(request, 'shipping/shipping.html', {'form': form})
 
 
 def checkout(request):

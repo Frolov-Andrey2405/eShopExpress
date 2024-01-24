32682d39b8e435d5dedcd6b45f5ae4cd78a09dbf
Andrey Frolov <senior.slender2015@gmail.com>
2024-01-24 18:14:40 +0200

    Webhook Stripe

--- eshopexpress/payment/views.py
+++ eshopexpress/payment/views.py
@@ -60,19 +60,20 @@ def complete_order(request):
         cart = Cart(request)
         total_price = cart.get_total_price()
 
+        shipping_address, _ = ShippingAddress.objects.get_or_create(
+            user=request.user,
+            defaults={
+                'name': name,
+                'email': email,
+                'street_address': street_address,
+                'apartment_address': apartment_address,
+                'country': country,
+                'zip': zip
+            }
+        )
+
         match payment_type:
             case 'stripe-payment':
-                shipping_address, _ = ShippingAddress.objects.get_or_create(
-                    user=request.user,
-                    defaults={
-                        'name': name,
-                        'email': email,
-                        'street_address': street_address,
-                        'apartment_address': apartment_address,
-                        'country': country,
-                        'zip': zip
-                    }
-                )
                 session_data = {
                     'mode': 'payment',
                     'success_url': request.build_absolute_uri(
@@ -111,11 +112,12 @@ def complete_order(request):
                             },
                             'quantity': item['qty'],
                         })
+                    session_data['client_reference_id'] = order.id
+                    session = stripe.checkout.Session.create(
+                        **session_data
+                    )
+                    return redirect(session.url, code=303)
 
-                        session = stripe.checkout.Session.create(
-                            **session_data
-                        )
-                        return redirect(session.url, code=303)
                 else:
                     order = Order.objects.create(
                         shipping_address=shipping_address, amount=total_price)
@@ -127,6 +129,23 @@ def complete_order(request):
                             price=item['price'],
                             quantity=item['qty']
                         )
+                        session_data['line_items'].append({
+                            'price_data': {
+                                'unit_amount': int(
+                                    item['price'] * Decimal(100)
+                                ),
+                                'currency': 'usd',
+                                'product_data': {
+                                    'name': item['product']
+                                },
+                            },
+                            'quantity': item['qty'],
+                        })
+                    session_data['client_reference_id'] = order.id
+                    session = stripe.checkout.Session.create(
+                        **session_data
+                    )
+                    return redirect(session.url, code=303)
 
 
 def payment_success(request):
